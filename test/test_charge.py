9f0f853c59983585babe9a95c373aadfb45acc58
Joseph Schilz <joseph@schilz.org>
2018-09-17 00:15:51 -0700

    Test exceptions arising from bad requests.

--- test/test_charge.py
+++ test/test_charge.py
@@ -2,6 +2,8 @@ import unittest
 import os
 
 from pystrike.charge import make_charge_class
+from pystrike.exceptions import ConnectionException, \
+        ClientRequestException, ChargeNotFoundException
 
 strike_api_key = os.environ.get('STRIKE_TESTNET_API_KEY')
 strike_api_host = os.environ.get('STRIKE_TESTNET_HOST', 'api.dev.strike.acinq.co')
@@ -42,8 +44,22 @@ class ChargeTestCase(unittest.TestCase):
         # fairly long.
         self.assertGreater(len(charge.payment_request), 10)
 
+    def test_charge_not_found_exception(self):
 
+        with self.assertRaises(ChargeNotFoundException):
 
+            # Retrieve a charge from Strike with a non-existant charge
+            # id.
+            charge = self.ChargeClass.from_charge_id('ch_madeupchargeid')
 
+    def test_client_request_exception(self):
 
+        with self.assertRaises(ClientRequestException):
+
+            # Attempt to create a charge with a non-existant currency.
+            charge = self.ChargeClass(
+                    100,
+                    'non-existant-currency',
+                    'Charge creation note',
+                )
 
